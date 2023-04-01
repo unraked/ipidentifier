8b67c465ebe1ce8773c9bb7a9c15d27d7c73bdae
unranked <121068809+unraked@users.noreply.github.com>
2023-04-01 15:26:17 -0600

    Create identifier.py

---
+++ identifier.py
@@ -0,0 +1,25 @@
+direccion_ip = input("Inserte Aquí Su IP")
+
+#Separa los números de la dirección
+
+Octeto = direccion_ip.split(".")
+
+#verifica la clase de su Dirección Ip
+
+If int(Octeto[0]) >= 0 and int(Octeto[0]) <= 126:
+print("Su Dirección IP es de Clase A")
+
+elif int(Octeto[0]) >= 127 and int(Octeto[0]) <= 191:
+print("Su Dirección IP es de Clase B")
+
+elif int(Octeto[0]) >= 192 and int(Octeto[0]) <= 223:
+print("Su Dirección IP es de Clase C")
+
+elif int(Octeto[0]) >= 224 and int(Octeto[0]) <= 239:
+print("Su Dirección IP es de Clase B")
+
+elif int(Octeto[0]) >= 240 and int(Octeto[0]) <= 255:
+print("Su Dirección IP es de Clase E")
+
+else:
+print("Su IP no es Válida")
